73b2d43048289af75e6f748c9a440562cd7ef308
RbCaViTerminal <robert@robertvail.info>
2023-10-31 10:12:09 -0400

    parser.__init__

---
+++ parsers/parser/__init__.py
@@ -0,0 +1,27 @@
+def defaultbacktrack(state):
+	return False
+
+def defaultgetvalue(state):
+	try:
+		return state.value
+	except AttributeException:
+		return None
+
+class Parser:
+	def __init__(self,parse,backtrack=defaultbacktrack,getvalue=defaultgetvalue):
+		self.parse=parse
+		self.backtrack=backtrack
+		self.getvalue=getvalue
+
+class ParserState:
+	def __init__(self,s):
+		if type(s)==str:
+			self.s=s
+		else:
+			self.s=s.s[s.length:]
+		self.length=0
+
+def parse(parser,s):
+	state=ParserState(s)
+	result=parser.parse(state)
+	return result,state
