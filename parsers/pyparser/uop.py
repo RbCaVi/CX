cc92674749d81846e7c3fdc44b09f6d38f86d727
RbCaViTerminal <robert@robertvail.info>
2023-11-04 15:16:28 -0400

    rewrite uopparser

--- parsers/pyparser/uop.py
+++ parsers/pyparser/uop.py
@@ -1,28 +1,5 @@
-from pyparser.parser import Parser,parse,called
-from pyparser.basicparsers import stripped,transform
+from pyparser.basicparsers import stripped,alternate,strp
 
 uops=['+','-']
 
-@stripped
-def unaryop():
-class UnaryOpParser():
-	def parse(self,state):
-		for i,uop in enumerate(uops):
-			if state.s.startswith(uop):
-				state.value=uop
-				state.i=i
-				state.length=len(uop)
-				return True
-		return False
-	def backtrack(self,state):
-		i=state.i
-		while i<uops.length:
-			uop=uops[i]
-			if state.s.startswith(uop):
-				state.value=uop
-				state.i=i
-				state.length=len(uop)
-				return True
-			i+=1
-		return False
-	return Parser(parseuop,backtrackuop)
+unaryop=stripped(alternate(strp(uop) for uop in uops))
