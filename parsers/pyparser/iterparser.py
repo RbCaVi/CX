adfa820d54be244fa55b37c9af7c05805abfb112
RbCaViTerminal <robert@robertvail.info>
2023-11-09 12:27:57 -0500

    they work now

--- parsers/pyparser/iterparser.py
+++ parsers/pyparser/iterparser.py
@@ -4,35 +4,68 @@ class Parser:
 	def getvalue(self,state):
 		try:
 			return state.value
-		except AttributeException:
+		except AttributeError:
 			return None
+	def __repr__(self):
+		return f'{self.__class__.__name__}()'
+
+class ParserState:
+	def __init__(self,s,value=None):
+		if type(s)==str:
+			self.s=s
+		else:
+			self.s=s.s
+		if value is not None:
+			self.value=value
+
+	def advance(self,length):
+		try:
+			return ParserState(self.s[length:],self.value)
+		except AttributeError:
+			return ParserState(self.s[length:])
+	def __repr__(self):
+		try:
+			return f'ParserState({repr(self.s)},{repr(self.value)})'
+		except AttributeError:
+			return f'ParserState({repr(self.s)})'
 
 class AlternateParser(Parser):
 	def __init__(self,*parsers):
 		self.parsers=parsers
 	def parse(self,state):
-		for i,p in enumerate(parsers):
-			for state in p.parse(state):
-				yield i,state
+		for i,p in enumerate(self.parsers):
+			for pstate in p.parse(state):
+				print(pstate)
+				out=ParserState(pstate.s)
+				out.state=pstate
+				out.i=i
+				yield out
+	def getvalue(self,state):
+		return state.i,self.parsers[state.i].getvalue(state.state)
+	def __repr__(self):
+		return f'{self.__class__.__name__}({",".join(map(repr,self.parsers))})'
+
 
 import itertools
+import functools
 
 class ConcatParser(Parser):
 	def __init__(self,*parsers):
 		self.parsers=parsers
 	def parse(self,state):
-		states=[[state]]
-		for p in parsers:
-			def f(ss):
-				for s in p.parse(ss[-1]):
-					yield ss+[s]
-			states=itertools.chain.from_iterable(map(f,states))
+		states=[[state]] # list of lists of states
+		f=lambda p,ss:[ss+[s] for s in p.parse(ss[-1])]
+		for p in self.parsers:
+			states=itertools.chain.from_iterable(map(functools.partial(f,p),states))
 		def f2(ss):
-
-			ParserState(ss[-1])
+			s=ParserState(ss[-1].s)
+			s.states=ss
+			return s
 		return map(f2,states)
 	def getvalue(self,state):
-		return map(f,parser.getvalue(state))
+		return [p.getvalue(s) for p,s in zip(self.parsers,state.states[1:])]
+	def __repr__(self):
+		return f'{self.__class__.__name__}({",".join(map(repr,self.parsers))})'
 
 class TransformParser(Parser):
 	def __init__(self,parser,f):
@@ -42,3 +75,13 @@ class TransformParser(Parser):
 		return parser.parse(state)
 	def getvalue(self,state):
 		return map(f,parser.getvalue(state))
+	def __repr__(self):
+		return f'{self.__class__.__name__}({repr(self.parser)},{repr(self.f)})'
+
+class StrSetParser(Parser):
+	def __init__(self,ss):
+		self.ss=ss
+	def parse(self,state):
+		yield from map(lambda s:ParserState(state,s).advance(len(s)),filter(state.s.startswith,self.ss))
+	def __repr__(self):
+		return f'{self.__class__.__name__}({repr(self.ss)})'
--- parsers/testparsers.py
+++ parsers/testparsers.py
@@ -1,6 +1,32 @@
-from pyparser.parser import parse
-from pyparser.basicparsers import parse
-from pyparser.uop import unaryop
+#from pyparser.parser import parse
+#from pyparser.basicparsers import parse
+#from pyparser.uop import unaryop
 
-result,state=parse(unaryop,'*')
-result,state=parse(unaryop,'8')
+#result,state=parse(unaryop,'*')
+#result,state=parse(unaryop,'8')
+
+from pyparser.iterparser import StrSetParser,ParserState,ConcatParser,AlternateParser
+
+def test(p,ss):
+	for s in ss:
+		print(f'parsing {repr(s)} with {repr(p)}')
+		it=p.parse(ParserState(s))
+		for x in it:
+			print('value',repr(p.getvalue(x)))
+			print('state',repr(x))
+		print()
+
+
+p=StrSetParser(['6','5','65'])
+
+#test(p,['6','65','5'])
+
+p1=StrSetParser(['7'])
+p2=StrSetParser(['5'])
+p=ConcatParser(p1,p2)
+
+test(p,['75','7','5','77','55'])
+
+p=AlternateParser(p1,p2)
+
+#test(p,['75','7','5'])
